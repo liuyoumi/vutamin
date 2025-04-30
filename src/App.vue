6b4a25b25252b9172ba8c9b204b324c0d3614346
liuyoumi <liuyoumi@hotmail.com>
2025-04-30 18:51:32 +0800

    feat: dynamic display layout component.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import Layout from "@/layout/Layout.vue";
+import {useRoute} from "vue-router";
+
+const route = useRoute();
+const isLayout = computed(() => route.meta.layout);
 </script>
 
 <template>
   <t-config-provider>
-    <Layout/>
+    <Layout v-if="isLayout"/>
+    <router-view v-else/>
   </t-config-provider>
 </template>
