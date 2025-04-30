769a05ef8e7ba6a00dfded6154b48a460630ef97
liuyoumi <liuyoumi@hotmail.com>
2025-04-30 18:47:59 +0800

    feat: add login method.

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -1,6 +1,9 @@
 import request from "@/share/request.js";
 
 export const AuthApi = {
+  login(data) {
+    return request.post("/auth/login", data);
+  },
   getProfile() {
     return request.get("/auth/profile");
   },
