e3cb8ceb3b45a7ed9dc9c379cffa1f1f3f99ad7b
liuyoumi <liuyoumi@hotmail.com>
2025-07-22 11:00:05 +0800

    feat: add radio type.

--- src/components/DictSelect/src/index.vue
+++ src/components/DictSelect/src/index.vue
@@ -23,4 +23,7 @@ const options = computed(() => {
 
 <template>
   <t-select v-if="selectType === 'select'" :options="options" v-bind="$attrs"/>
+  <t-radio-group v-else-if="selectType === 'radio'" variant="primary-filled" v-bind="$attrs">
+    <t-radio-button v-for="item in options" :key="item.value" :value="item.value" :label="item.label"/>
+  </t-radio-group>
 </template>
