8befcdc47863d8c9aef0df8c28a5708034053285
liuyoumi <liuyoumi@hotmail.com>
2025-07-31 11:33:42 +0800

    feat<redirect>: refresh page.

---
+++ src/views/redirect/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {useRoute, useRouter} from "vue-router";
+
+defineOptions({name: "Redirect"});
+
+const route = useRoute();
+const router = useRouter();
+const {params, query} = route;
+
+router.replace({
+  path: params.path.startsWith("/") ? params.path : `/${params.path}`,
+  query,
+});
+</script>
+
+<template>
+  <div></div>
+</template>
